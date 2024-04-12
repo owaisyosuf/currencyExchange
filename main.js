1bb10e98d00cf1f95f1613c15f804bb2b3a42add
owaisyosuf <157351741+owaisyosuf@users.noreply.github.com>
2024-04-12 15:25:57 +0500

    currency exchange project

---
+++ main.js
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+};
+let answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter From Currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+    },
+    {
+        name: "to",
+        message: "Enter To Currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+    },
+    {
+        name: 'amount',
+        message: 'enter your amount',
+        type: 'number'
+    }
+]);
+let fromCurrency = currency[answer.from];
+let toCurrency = currency[answer.to];
+let userAmount = answer.amount;
+let basedCurrency = userAmount / fromCurrency;
+let convertedAmount = basedCurrency * toCurrency;
+// console.log(fromCurrency);
+// console.log(toCurrency);
+// console.log(userAmount);
+// console.log(basedCurrency);
+console.log(Math.floor(convertedAmount));
